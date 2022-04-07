2c274e2bb496328fdc523e2488c2485cca561239
Kévin Dérécusson <derec84@hotmail.fr>
2022-04-07 22:54:31 +0200

    Use csv data to create Stock object list

---
+++ bruteforce.py
@@ -0,0 +1,22 @@
+import csv
+
+
+stocks_list = []
+
+class Stock:
+    def __init__(self, name, cost, profitability):
+        self.name = name
+        self.cost = int(cost)
+        self.profitability = int(profitability)
+        self.profit = round(self.cost * (self.profitability / 100), 2)
+
+
+with open("data.csv", newline="") as file:
+    has_header = csv.Sniffer().has_header(file.read(1024))
+    file.seek(0)
+    reader = csv.reader(file)
+    if has_header:
+        next(reader)
+    for row in reader:
+        stock = Stock(row[0], row[1], row[2])
+        stocks_list.append(stock)
