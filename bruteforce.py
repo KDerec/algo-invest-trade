b88963443424eddab8dc76cf38ca1c04ebe3fdb5
Kévin Dérécusson <derec84@hotmail.fr>
2022-04-07 22:58:29 +0200

    Add counter to control which data rows to process

--- bruteforce.py
+++ bruteforce.py
@@ -3,6 +3,7 @@ import csv
 
 stocks_list = []
 
+
 class Stock:
     def __init__(self, name, cost, profitability):
         self.name = name
@@ -17,6 +18,10 @@ with open("data.csv", newline="") as file:
     reader = csv.reader(file)
     if has_header:
         next(reader)
+    i = 0
     for row in reader:
         stock = Stock(row[0], row[1], row[2])
         stocks_list.append(stock)
+        i += 1
+        if i == 10:
+            break
