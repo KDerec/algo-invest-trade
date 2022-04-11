f76f139276c4d36a62e17e72baf7009d78815fef
Kévin Dérécusson <derec84@hotmail.fr>
2022-04-11 13:51:32 +0200

    Found best profit with bruteforce algo

--- bruteforce.py
+++ bruteforce.py
@@ -5,48 +5,59 @@ from datetime import datetime
 start_timer = datetime.now()
 
 
+class Stock:
+    def __init__(self, name, cost, profitability):
+        self.name = name
+        self.cost = int(cost)
+        self.profitability = int(profitability)
+        self.profit = round(self.cost * (self.profitability / 100), 2)
+
+
 with open("data.csv", newline="") as file:
     has_header = csv.Sniffer().has_header(file.read(1024))
     file.seek(0)
     reader = csv.reader(file)
     if has_header:
         next(reader)
-    i = 0
     stocks_list = []
     for row in reader:
         name = row[0]
-        cost = int(row[1])
-        profitability = int(row[2])
-        profit = round(cost * (profitability / 100), 2)
-
-        stock = (name, cost, profit)
+        cost = row[1]
+        profitability = row[2]
+        stock = Stock(name, cost, profitability)
         stocks_list.append(stock)
-        i += 1
-        if i == 20:
-            break
 
 
-def greedy_algo(capital, stocks_list):
-    sorted_stocks_list = sorted(stocks_list, key=lambda x: x[2])
-    selected_stock = []
-    total_cost = 0
+def bruteforce_algo(capital, stocks_list, stocks_selection=[]):
+    if stocks_list:
+        profit_solution1, stocks_list_solution1 = bruteforce_algo(
+            capital, stocks_list[1:], stocks_selection
+        )
+        selected_stock = stocks_list[0]
+        if selected_stock.cost <= capital:
+            profit_solution2, stocks_list_solution2 = bruteforce_algo(
+                capital - selected_stock.cost,
+                stocks_list[1:],
+                stocks_selection + [selected_stock],
+            )
+            if profit_solution1 < profit_solution2:
+                return profit_solution2, stocks_list_solution2
 
-    while sorted_stocks_list:
-        stock = sorted_stocks_list.pop()
-        if stock[1] + total_cost <= capital:
-            selected_stock.append(stock)
-            total_cost += stock[1]
-
-    total_profit = sum([i[2] * 100 for i in selected_stock]) / 100
-
-    return total_profit, total_cost, selected_stock
+        return profit_solution1, stocks_list_solution1
+    else:
+        best_profit = sum([stock.profit * 100 for stock in stocks_selection]) / 100
+        return best_profit, stocks_selection
 
 
-total_profit, total_cost, selected_stock = greedy_algo(500, stocks_list)
+capital = 500
 
-print(f"Le bénéfice est de {total_profit}€")
-print(f"Le coût total est de {total_cost}€")
-print(f"Liste des actions sélectionnées :\n{selected_stock}")
+best_profit, stocks_result = bruteforce_algo(capital, stocks_list)
+list_to_display = []
+for stock in stocks_result:
+    list_to_display.append(stock.name)
+
+print(f"Le montant du bénéfice est de {best_profit}€.")
+print(f"La liste d'action est : {list_to_display}")
 
 end_timer = datetime.now()
 
