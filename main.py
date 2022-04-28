cc1a413c35a430b1948e8b544020c49c9cdc9b33
Kévin Dérécusson <derec84@hotmail.fr>
2022-04-28 21:57:14 +0200

    Plot time complexity graphs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from time import perf_counter
 from controller import create_stocks_list
 from view import display_result_information
@@ -6,10 +7,21 @@ from optimized import optimized_algo
 
 
 capital = 500
-stocks_list = create_stocks_list()
 
 
 def main():
+    choice = input(
+        "Taper 'o' pour afficher un graphique, sinon, trouve le meilleur résultat: "
+    )
+    if choice.upper() == "O":
+        plot_graphs()
+    else:
+        found_the_best_result()
+
+
+def found_the_best_result():
+    stocks_list = create_stocks_list()
+
     start_timer = perf_counter()
     bruteforce_best_profit, bruteforce_stocks_result = bruteforce_algo(
         capital, stocks_list
@@ -33,5 +45,42 @@ def main():
     )
 
 
+def plot_graphs():
+    number_of_stock = []
+    bruteforce_times = []
+    optimized_times = []
+
+    for i in range(1, 21):
+        stocks_list = create_stocks_list(i)
+
+        start_timer = perf_counter()
+        bruteforce_best_profit, bruteforce_stocks_result = bruteforce_algo(
+            capital, stocks_list
+        )
+        end_timer = perf_counter()
+
+        bruteforce_times.append(end_timer - start_timer)
+
+        start_timer = perf_counter()
+        optimized_best_profit, optimized_stocks_result = optimized_algo(
+            capital, stocks_list
+        )
+        end_timer = perf_counter()
+
+        optimized_times.append(end_timer - start_timer)
+
+        number_of_stock.append(i)
+
+    plt.figure().canvas.manager.set_window_title("Time Complexity")
+    plt.xlabel("Number of stock")
+    plt.ylabel("Execution Time (s)")
+    plt.plot(number_of_stock, bruteforce_times, label="bruteforce_algo()")
+    plt.plot(number_of_stock, optimized_times, label="optimized_algo()")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == "__main__":
     main()
+    exit()
