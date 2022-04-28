a878150f8ff2a8f53b32dc079877aead3b7cd5a3
Kévin Dérécusson <derec84@hotmail.fr>
2022-04-28 21:53:01 +0200

    Add better result display

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
     )
     end_timer = perf_counter()
     bruteforce_time = end_timer - start_timer
+    print("Le résultat pour l'algorithme bruteforce est : ")
     display_result_information(
         bruteforce_best_profit, bruteforce_stocks_result, bruteforce_time
     )
@@ -26,6 +27,7 @@ def main():
     )
     end_timer = perf_counter()
     optimized_time = end_timer - start_timer
+    print("Le résultat pour l'algorithme optimisé est : ")
     display_result_information(
         optimized_best_profit, optimized_stocks_result, optimized_time
     )
--- view.py
+++ view.py
@@ -7,8 +7,7 @@ def display_result_information(best_profit, stocks_result, time):
     spent_capital = create_spent_capital(stocks_result)
 
     print(
-        f"""
-    Le montant du bénéfice est de {best_profit}€.
+        f"""    Le montant du bénéfice est de {best_profit}€.
     La liste d'action est : {stocks_name_list}.
     Le montant de l'investissement est de {spent_capital}€.
     Temps d'exécution du programme : {time} secondes.
